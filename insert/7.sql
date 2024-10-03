7814261ded31b07fd205d600d15d97231d6601a7
unknown <123@gmail.com>
2024-10-03 09:18:39 +0300

    db design

---
+++ insert/7.sql
@@ -0,0 +1,4 @@
+use sql_store;
+
+insert into customers(first_name,last_name,birth_date,phone,address,city,state,points)
+values ('Janis','berzins',2000-05-05, 25697445, 'talinas iela 13','riga','CA',10); 
